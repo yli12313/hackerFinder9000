c133c9b76d4413f9f6678b010c574e9fe0ca792a
hackhackhack <apart@hack.now>
2025-11-23 20:38:59 +0000

    fix: update docke run command

--- lambda_launch.sh
+++ lambda_launch.sh
@@ -5,12 +5,17 @@ export HF_HOME="/home/ubuntu/.cache/huggingface"
 export MODEL_REPO=huihui-ai/Llama-3.3-70B-Instruct-abliterated
 
 sudo docker run \
-        --gpus all \
-        --ipc=host \
-        -v "${HF_HOME}":/root/.cache/huggingface \
-        -p 8000:8000 \
-        --env "HUGGING_FACE_HUB_TOKEN=${HF_TOKEN}" \
-        vllm/vllm-openai --model "${MODEL_REPO}" \
-        --disable-log-requests \
-        --tensor-parallel-size 8
-
+      --gpus all \
+      --ipc=host \
+      -v "${HF_HOME}":/root/.cache/huggingface \
+      -p 8001:8000 \
+      --env "HUGGING_FACE_HUB_TOKEN=${HF_TOKEN}" \
+      vllm/vllm-openai:latest \
+      --model huihui-ai/Huihui-Qwen3-VL-30B-A3B-Instruct-abliterated \
+      --disable-log-requests \
+      --tensor-parallel-size 2 \
+      --enable-expert-parallel \
+      --mm-encoder-tp-mode data \
+      --max-model-len 32768 \
+      --enable-auto-tool-choice \
+      --tool-call-parser hermes
